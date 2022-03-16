0d8698066e7b3898b37c9cc069c8768bbbc3fb89
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-16 18:17:26 -0500

    Requisito 5 - Reduzindo codigo e corrigindo lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -4,35 +4,31 @@ const allAnimals = () => {
   const animals = {};
 
   data.species.map((element) => Object.assign(animals, {
-    [element.name]: element.residents.length
+    [element.name]: element.residents.length,
   }));
 
   return animals;
-}
+};
 
 const onlySpecie = (specie) => {
   const animals = data.species.find((element) => element.name === specie);
   return animals;
-}
+};
 
 const specieAndSex = (specie, sex) => {
   const animals = onlySpecie(specie).residents.filter((element) => element.sex === sex);
 
   return animals.length;
-}
+};
 
 function countAnimals(animal = 0) {
-
-  if (animal === 0) {
-    return allAnimals();
-  }
   if (Object.keys(animal).length === 1) {
     return onlySpecie(animal.specie).residents.length;
   }
   if (Object.keys(animal).length === 2) {
     return specieAndSex(animal.specie, animal.sex);
   }
-
+  return allAnimals();
 }
 
 module.exports = countAnimals;
