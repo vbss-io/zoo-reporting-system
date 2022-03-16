f3de99bcc45bde0f27249d76f588f88ed8422638
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-16 13:47:46 -0500

    Requisito 2 - Adicionando Every para verificar a idade

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,20 +2,21 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   const object = data.species
+  const animalData = object.find((element) => element.name === animal);
 
-  return object.find((element) => element.name === animal);
+  return animalData.residents.every((element) => element.age >= age);
 }
 
 // console.log(getAnimalsOlderThan('boi', 10));
 // console.log(data.species[0].residents)
 
 // const test = data.species.find((element) => element.name === 'lions');
+// const test2 = test.residents.every((element) => element.age >= 7);
 
 // const test = data.species.forEach((item) => console.log(item.residents));
 
-// console.log(test);
+// console.log(test2);
 
-console.log(getAnimalsOlderThan('otters', 10));
-
+// console.log(getAnimalsOlderThan('otters', 8));
 
 module.exports = getAnimalsOlderThan;
