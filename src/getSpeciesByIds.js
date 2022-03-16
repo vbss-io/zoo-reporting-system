ea9e2060c11cf064b3a17d85ea07713bc856620d
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-16 13:24:39 -0500

    Requisito 1 - Reduzindo código e corrigindo lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,15 +2,7 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   const object = data.species;
-  // Acessando primeiro ID
-  // return data.species.filter((element) => element.id === ids);
-  // Acessando todos elementos ID
-  return object.filter((elementFilter) => ids.find((elementFind) => elementFind === elementFilter.id));
+  return object.filter((element) => ids.find((elementFind) => elementFind === element.id));
 }
-
-// console.log(getSpeciesByIds());
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'baa6e93a-f295-44e7-8f70-2bcdc6f6948d'))
-console.log(getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d'))
-  // console.log(getSpeciesByIds('78460a91-f4da-4dea-a469-86fd2b8ccc84'))
 
 module.exports = getSpeciesByIds;
