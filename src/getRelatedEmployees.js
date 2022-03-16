06f42112f5223b1d1ef711837a41d0085ee000aa
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-16 15:52:31 -0500

    Requisito 4 - Implementando função getManagers pra encontrar no data os gerentes

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,5 +1,14 @@
 const data = require('../data/zoo_data');
 
+function getManagers() {
+  // new Set(...) foi uma alternativa encontrada no google e baseada pelo mdn
+  // usado para remover itens repetidos
+  let managers = [];
+  data.employees.forEach((element) => element.managers.forEach((item) => managers.push(item)));
+  return new Set(managers);
+}
+
+
 function isManager(id) {
   // seu código aqui
 }
@@ -7,5 +16,7 @@ function isManager(id) {
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
+
+console.log(getManagers());
 
 module.exports = { isManager, getRelatedEmployees };
