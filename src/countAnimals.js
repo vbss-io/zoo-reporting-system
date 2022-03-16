65280c5db513a564859e853e5aad7ae826f7dab0
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-16 18:06:34 -0500

    Requisito 5 - Implementando countAnimals apenas com especie

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -10,15 +10,23 @@ const allAnimals = () => {
   return animals;
 }
 
+const onlySpecie = (specie) => {
+  const animals = data.species.find((element) => element.name === specie);
+  return animals.residents.length;
+}
+
 function countAnimals(animal = 0) {
 
   if (animal === 0) {
     return allAnimals();
   }
+  if (Object.keys(animal).length === 1) {
+    return onlySpecie(animal.specie);
+  }
 
 }
 
-
-console.log(countAnimals());
+console.log(countAnimals({ specie: 'penguins' }));
+// console.log(countAnimals());
 
 module.exports = countAnimals;
