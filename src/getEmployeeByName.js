5d232a639a34a961fa90f33e9e794526b6ba2984
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-16 13:58:32 -0500

    Requisito 3 - Buscando funcionario pelo primeiro nome

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  // Implementando mesma logica do requisto 2
+  const object = data.employees;
+
+  return object.find((element) => element.firstName === employeeName);
 }
+
+// console.log(data.employees)
+
+console.log(getEmployeeByName('Nigel'))
 
 module.exports = getEmployeeByName;
