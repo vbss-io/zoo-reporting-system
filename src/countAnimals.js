68e5aa9bd52f2d7bcfddc57ab57480577734ea89
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-16 18:13:50 -0500

    Requisito 5 - Implementando countAnimals para especie e sexo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -12,7 +12,13 @@ const allAnimals = () => {
 
 const onlySpecie = (specie) => {
   const animals = data.species.find((element) => element.name === specie);
-  return animals.residents.length;
+  return animals;
+}
+
+const specieAndSex = (specie, sex) => {
+  const animals = onlySpecie(specie).residents.filter((element) => element.sex === sex);
+
+  return animals.length;
 }
 
 function countAnimals(animal = 0) {
@@ -21,12 +27,12 @@ function countAnimals(animal = 0) {
     return allAnimals();
   }
   if (Object.keys(animal).length === 1) {
-    return onlySpecie(animal.specie);
+    return onlySpecie(animal.specie).residents.length;
+  }
+  if (Object.keys(animal).length === 2) {
+    return specieAndSex(animal.specie, animal.sex);
   }
 
 }
-
-console.log(countAnimals({ specie: 'penguins' }));
-// console.log(countAnimals());
 
 module.exports = countAnimals;
