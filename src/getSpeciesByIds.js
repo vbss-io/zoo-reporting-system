feaf4eea22a3ea2d56d7181d7ea98148b317b3c6
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-16 13:21:01 -0500

    Requisito 1 - Pegando mais de um ID com rest, filter e find

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,13 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
+  const object = data.species;
   // Acessando primeiro ID
-  return data.species.find((element) => element.id === ids);
+  // return data.species.filter((element) => element.id === ids);
+  // Acessando todos elementos ID
+  return object.filter((elementFilter) => ids.find((elementFind) => elementFind === elementFilter.id));
 }
 
-// console.log(data.species[0]);
-// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
-// console.log(getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d'))
-// console.log(getSpeciesByIds('78460a91-f4da-4dea-a469-86fd2b8ccc84'))
+// console.log(getSpeciesByIds());
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'baa6e93a-f295-44e7-8f70-2bcdc6f6948d'))
+console.log(getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d'))
+  // console.log(getSpeciesByIds('78460a91-f4da-4dea-a469-86fd2b8ccc84'))
 
 module.exports = getSpeciesByIds;
