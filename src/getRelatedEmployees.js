f331c84adb7f3c7549837b43dee46652ae45443e
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-16 16:17:52 -0500

    Requisito 4 - Implementando função isManager

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,35 +1,24 @@
 const data = require('../data/zoo_data');
 
 function getManagers() {
+  // Remoção de itens repetidos consultado no site DelftStack
   let managers = [];
   data.employees.forEach((element) => element.managers.forEach((item) => managers.push(item)));
   return managers.filter((element, index) => managers.indexOf(element) === index);
 }
 
 function isManager(id) {
-  // seu código aqui
+  const managers = getManagers();
+  return managers.includes(id);
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
 
-// let managers = [];
+// console.log(getManagers().includes('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 
-console.log(getManagers().includes('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
-
-// managers = managers.filter((este, i) => managers.indexOf(este) === i);
-
-// const test2 = test.map((element) => element);
-
-// const test2 = test.forEach((element, index) => console.log(element[index]));
-
-// const test2 = data.employees[0].managers;
-
-// console.log(test);
-// console.log(managers);
-// console.log(test2)
-
+console.log(isManager('b0dc644a-5335-489b-8a2c-4e086c7819aa2'))
 
 
 
