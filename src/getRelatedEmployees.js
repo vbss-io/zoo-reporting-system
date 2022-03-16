042bcd47023caa0167b6b65bc68a6f14fe5a6a20
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-16 16:32:43 -0500

    Requisito 4 - Reduzindo codigo e corrigindo lint

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,8 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getManagers() {
-  // Remoção de itens repetidos consultado no site DelftStack
-  let managers = [];
+  // Remoção de itens repetidos consultado no site DelftStack usando filter
+  const managers = [];
   data.employees.forEach((element) => element.managers.forEach((item) => managers.push(item)));
   return managers.filter((element, index) => managers.indexOf(element) === index);
 }
@@ -20,19 +20,5 @@ function getRelatedEmployees(managerId) {
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
-// console.log(getManagers().includes('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
-
-// console.log(isManager('b0dc644a-5335-489b-8a2c-4e086c7819aa2'))
-
-// const test = data.employees.filter((person) => person.managers.includes('b0dc644a-5335-489b-8a2c-4e086c7819a2'))
-// const test2 = test.map((person) => `${person.firstName} ${person.lastName}`);
-
-// console.log(test);
-// console.log(test2);
-
-// console.log(getRelatedEmployees('b0dc644a-5335-489b-8a2c-4e086c7819a2a'))
-
-
 
 module.exports = { isManager, getRelatedEmployees };
