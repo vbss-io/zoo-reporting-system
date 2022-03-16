5025aa646a0dc5dd43f80bab98e333b9d9a4b4f4
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-16 13:49:03 -0500

    Requisito 2 - Reduzindo codigo e corrigindo lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,22 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const object = data.species
+  const object = data.species;
   const animalData = object.find((element) => element.name === animal);
 
   return animalData.residents.every((element) => element.age >= age);
 }
-
-// console.log(getAnimalsOlderThan('boi', 10));
-// console.log(data.species[0].residents)
-
-// const test = data.species.find((element) => element.name === 'lions');
-// const test2 = test.residents.every((element) => element.age >= 7);
-
-// const test = data.species.forEach((item) => console.log(item.residents));
-
-// console.log(test2);
-
-// console.log(getAnimalsOlderThan('otters', 8));
 
 module.exports = getAnimalsOlderThan;
