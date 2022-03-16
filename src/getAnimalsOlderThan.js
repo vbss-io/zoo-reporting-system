a621077823129627723499e05131c2817f2b9be4
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-16 13:42:23 -0500

    Requisito 2 - Encontrando o objeto referente ao animal

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const object = data.species
+
+  return object.find((element) => element.name === animal);
 }
+
+// console.log(getAnimalsOlderThan('boi', 10));
+// console.log(data.species[0].residents)
+
+// const test = data.species.find((element) => element.name === 'lions');
+
+// const test = data.species.forEach((item) => console.log(item.residents));
+
+// console.log(test);
+
+console.log(getAnimalsOlderThan('otters', 10));
+
 
 module.exports = getAnimalsOlderThan;
