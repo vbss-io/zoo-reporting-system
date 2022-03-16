9b6ad654b0d6b92768493ee0e22fef49a8af58c3
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-16 16:31:36 -0500

    Requisito 4 - Implementando getRelatedEmployees()

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -13,12 +13,25 @@ function isManager(id) {
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const manager = isManager(managerId);
+  if (manager === true) {
+    const managed = data.employees.filter((person) => person.managers.includes(managerId));
+    return managed.map((person) => `${person.firstName} ${person.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 // console.log(getManagers().includes('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 
-console.log(isManager('b0dc644a-5335-489b-8a2c-4e086c7819aa2'))
+// console.log(isManager('b0dc644a-5335-489b-8a2c-4e086c7819aa2'))
+
+// const test = data.employees.filter((person) => person.managers.includes('b0dc644a-5335-489b-8a2c-4e086c7819a2'))
+// const test2 = test.map((person) => `${person.firstName} ${person.lastName}`);
+
+// console.log(test);
+// console.log(test2);
+
+// console.log(getRelatedEmployees('b0dc644a-5335-489b-8a2c-4e086c7819a2a'))
 
 
 
