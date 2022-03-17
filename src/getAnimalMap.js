9db0c6ce3756ae1b630b10afa4ebef4abe7c010b
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-17 14:30:49 -0500

    Requisito 7 - Implementando getAnimalMap sem parametros

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const getAnimals = (location) => {
+  const animals = [];
+  data.species.filter((element) => element.location === location).forEach((element) => animals.push(element.name));
+  return animals;
 }
+
+const getLocation = () => {
+  const location = {}
+  data.species.forEach((element) => Object.assign(location, {
+    [element.location]: getAnimals(element.location),
+  }));
+  return location;
+}
+
+function getAnimalMap(options) {
+  if (options = 0) {
+    return getLocation();
+  }
+}
+
+
+
+// console.log(data.species[0])
+// console.log(getAnimals('NE'));
+
+console.log(getAnimalMap());
+
 
 module.exports = getAnimalMap;
