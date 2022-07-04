a8722dfe063c436d1a350a1438cd2e9c4eefc3e6
vitorbss12 <vitor_bss10@hotmail.com>
2022-07-04 11:04:20 -0300

    Requisito 9 - getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employees = data.employees;
+  const currentEmployee = employees.filter(employee => employee.id === id).map(employee => employee.responsibleFor);
+  const firstSpecie = currentEmployee[0][0];
+  const sortSpecies = data.species.filter(specie => specie.id === firstSpecie)[0].residents
+    .sort((a, b) => b.age - a.age);
+  const oldest = sortSpecies[0];
+  return [oldest.name, oldest.sex, oldest.age];
 }
+
+getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
+
 
 module.exports = getOldestFromFirstSpecies;
