f75dfabcd816fd4ea7ed8872200287cce3c9edc7
vitorbss12 <vitor_bss10@hotmail.com>
2022-07-04 11:37:33 -0300

    Corrigindo Lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,13 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  const employees = data.employees;
-  const currentEmployee = employees.filter(employee => employee.id === id).map(employee => employee.responsibleFor);
+  const { employees } = data;
+  const currentEmployee = employees
+    .filter((employee) => employee.id === id)
+    .map((employee) => employee.responsibleFor);
   const firstSpecie = currentEmployee[0][0];
-  const sortSpecies = data.species.filter(specie => specie.id === firstSpecie)[0].residents
+  const sortSpecies = data.species.filter((specie) => specie.id === firstSpecie)[0].residents
     .sort((a, b) => b.age - a.age);
-  const oldest = sortSpecies[0];
-  return [oldest.name, oldest.sex, oldest.age];
+  const { name, sex, age } = sortSpecies[0];
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
