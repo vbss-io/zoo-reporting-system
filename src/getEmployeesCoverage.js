e16ad37aea08f0e8843d22ebdaf16927ff3a8001
vitorbss12 <vitor_bss10@hotmail.com>
2022-07-04 11:58:40 -0300

    Requisito 10 - getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,49 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getAnimalsOrLocation(ids, key) {
+  const currentAnimals = [];
+  ids.forEach((id) => {
+    currentAnimals.push(data.species.find((item) => item.id === id)[key]);
+  });
+  return currentAnimals;
+}
+
+function checkEmployee(currentEmployee) {
+  if (currentEmployee) {
+    return currentEmployee;
+  }
+  throw new Error('Informações inválidas');
+}
+
+function getAllEmployees(currentEmployee) {
+  const allEmployees = [];
+  currentEmployee.forEach((employee) => {
+    allEmployees.push({
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: getAnimalsOrLocation(employee.responsibleFor, 'name'),
+      locations: getAnimalsOrLocation(employee.responsibleFor, 'location'),
+    });
+  });
+  return allEmployees;
+}
+
+function getEmployeesCoverage(search = null) {
+  const { employees } = data;
+  if (search) {
+    const { name, id } = search;
+    const currentEmployee = employees
+      .find((employee) => employee.firstName === name
+        || employee.lastName === name || employee.id === id);
+    const verifiedEmployee = checkEmployee(currentEmployee);
+    return {
+      id: verifiedEmployee.id,
+      fullName: `${verifiedEmployee.firstName} ${verifiedEmployee.lastName}`,
+      species: getAnimalsOrLocation(verifiedEmployee.responsibleFor, 'name'),
+      locations: getAnimalsOrLocation(verifiedEmployee.responsibleFor, 'location'),
+    };
+  }
+  return getAllEmployees(employees);
 }
 
 module.exports = getEmployeesCoverage;
