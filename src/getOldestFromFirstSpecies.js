111e1a8ecda23694d329bedeb393adb835637f15
vitorbss12 <vitor_bss10@hotmail.com>
2022-08-18 14:23:50 -0300

    refactor: move sort to a separete statement to fix code smell in getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -8,11 +8,9 @@ function getOldestFromFirstSpecies(id) {
   const firstSpecie = currentEmployee[0][0];
   const filterSpecies = zooData.species.filter((specie) => specie.id === firstSpecie);
   const residentsSpecies = filterSpecies[0].residents;
-  const sortSpecies = residentsSpecies.sort((a, b) => b.age - a.age);
-  const { name, sex, age } = sortSpecies[0];
+  residentsSpecies.sort((a, b) => b.age - a.age);
+  const { name, sex, age } = residentsSpecies[0];
   return [name, sex, age];
 }
-
-console.log(getOldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 export default getOldestFromFirstSpecies;
